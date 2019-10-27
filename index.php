ff22947ddb083c443fe87da1669aa347a00ba6bd
sonicdaw <mshd@da.mbn.or.jp>
2019-10-28 08:20:02 +0900

    Fix touch Hold Elevator not to move in case of person ride on

--- index.php
+++ index.php
@@ -435,8 +435,7 @@ window.onload = function() {
 
     // move elevator
     for(var i = 0; i < NUM_OF_ELEVATORS; i++){
-     if(touchHoldElevator != i){
-
+     if((touchHoldElevator != i && person_ride_on[i] == false) || person_ride_on[i] == true){
       if(elevator_y[i] > getFloorY(elevator_target_floor[i])) {elevator_y[i] -= elevator_vy[i];};
       if(elevator_y[i] < getFloorY(elevator_target_floor[i])) {elevator_y[i] += elevator_vy[i];};
 
