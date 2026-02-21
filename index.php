9484bcddcc723bef0b2a0504a105a71279b2fd44
sonicdaw <mshd@da.mbn.or.jp>
2026-02-21 16:02:50 +0900

    Replace building text with entaflip copyright for retail tech version

--- index.php
+++ index.php
@@ -522,9 +522,9 @@ window.onload = function() {
     ctx.beginPath();
     ctx.strokeStyle = "#eaa";
     ctx.font = "18pt 'Times New Roman'";
-    ctx.fillText("Smile Job Adventure", LEFT_OFFSET + FIELD_WIDTH / 2, TOP_OFFSET + (TOP_OFFSET + FIELD_HEIGHT / 8));
+    ctx.fillText("ENTAFLIP", LEFT_OFFSET + FIELD_WIDTH / 2, TOP_OFFSET + (TOP_OFFSET + FIELD_HEIGHT / 8));
     ctx.font = "17pt 'Times New Roman'";
-    ctx.fillText("Isle Shinagawa", LEFT_OFFSET + FIELD_WIDTH / 2, TOP_OFFSET + (TOP_OFFSET + FIELD_HEIGHT / 8 * 1.5));
+    ctx.fillText("Copyright(C) 2026", LEFT_OFFSET + FIELD_WIDTH / 2, TOP_OFFSET + (TOP_OFFSET + FIELD_HEIGHT / 8 * 1.5));
     ctx.stroke();
 
 
